cd5f5a4df4e7e89a2c8e7eef5a04e8eae0085a36
Nico <nicolas.romero.f@usach.cl>
2017-11-22 03:17:59 +0000

    fixed archivo

--- main.c
+++ main.c
@@ -32,88 +32,12 @@ typedef struct __attribute__((__packed__)) {
     unsigned char r;                                                                                                                                                                                                                        
 } IMAGE;
 
-<<<<<<< HEAD
-=======
+
 typedef struct matriz{
 	IMAGE ** matriz;
 	int x;
 	int y;
 } Matriz;
-
-unsigned char *LoadBitmapFile(char *filename, BITMAPINFOHEADER *bitmapInfoHeader)
-{
-    FILE *filePtr; //our file pointer
-    BITMAPFILEHEADER bitmapFileHeader; //our bitmap file header
-    unsigned char *bitmapImage;  //store image data
-    int imageIdx=0;  //image index counter
-    unsigned char tempRGB;  //our swap variable
->>>>>>> dbd495de91b25a99e9ede3ef16087296ddef815b
-
-
-int main() {                                                                                                                                                                                                                             
-    FILEHEADER fh;                                                                                                                                                                                                                           
-    INFOHEADER ih;                                                                                                                                                                                                                           
-    FILE *img = fopen("wea4.bmp", "rb");
-    fread(&fh, sizeof(unsigned char), sizeof(FILEHEADER), img);
-    fread(&ih, sizeof(unsigned char), sizeof(INFOHEADER), img);
-    printf("fM1 = %c, fM2 = %c, bfS = %u, un1 = %hu, un2 = %hu, iDO = %u\n", fh.fileMarker1, fh.fileMarker2, fh.bfSize, fh.unused1, fh.unused2, fh.imageDataOffset);                                                                         
-    printf("w = %d, h = %d, biSI = %d\n", ih.width, ih.height, ih.biSizeImage);
-    int i;
-    
-    IMAGE im;
-    for(i=0;i<9;i++)
-    {
-    	fread(&im, sizeof(unsigned char), sizeof(IMAGE), img);
-    	printf("r = %d, g = %d, b = %d\n", im.r, im.g, im.b);
-    }
-<<<<<<< HEAD
-    return 0;
-=======
-
-    //read in the bitmap image data
-    fread(bitmapImage,bitmapInfoHeader->biSizeImage,filePtr);
-
-    //make sure bitmap image data was read
-    if (bitmapImage == NULL)
-    {
-        fclose(filePtr);
-        printf("ERROR: No se puedo leer la informacion de la imagen\n");
-        return NULL;
-    }
-
-    //swap the r and b values to get RGB (bitmap is BGR)
-    for (imageIdx = 0;imageIdx < bitmapInfoHeader->biSizeImage;imageIdx+=3) // fixed semicolon
-    {
-        tempRGB = bitmapImage[imageIdx];
-        bitmapImage[imageIdx] = bitmapImage[imageIdx + 2];
-        bitmapImage[imageIdx + 2] = tempRGB;
-    }
-
-    //close file and return bitmap iamge data
-    fclose(filePtr);
-    return bitmapImage;
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 void insertarPixel(IMAGE * pixeles, int n, int x, int y, Matriz * matrizFinal){
 	int i,sumaR=0,sumaG=0,sumaB=0;
@@ -159,18 +83,48 @@ void reducirPorFilas(Matriz * matriz, Matriz * matrizFinal, int nPixeles){
 }
 
 
-int main()
-{
-	BITMAPINFOHEADER bitmapInfoHeader;
-	unsigned char *bitmapData;
-	bitmapData = LoadBitmapFile(".bmp",&bitmapInfoHeader);
-	
-	int i =0;
-	for(i=0;i<100;i++)
-	{
-		printf("%c\n",bitpamData[i];
-	}
-	return 0;
-//now do what you want with it, later on i will show you how to
->>>>>>> dbd495de91b25a99e9ede3ef16087296ddef815b
+
+
+int main() {                                                                                                                                                                                                                             
+    FILEHEADER fh;                                                                                                                                                                                                                           
+    INFOHEADER ih;                                                                                                                                                                                                                           
+    FILE *img = fopen("wea4.bmp", "rb");
+    fread(&fh, sizeof(unsigned char), sizeof(FILEHEADER), img);
+    fread(&ih, sizeof(unsigned char), sizeof(INFOHEADER), img);
+    printf("fM1 = %c, fM2 = %c, bfS = %u, un1 = %hu, un2 = %hu, iDO = %u\n", fh.fileMarker1, fh.fileMarker2, fh.bfSize, fh.unused1, fh.unused2, fh.imageDataOffset);                                                                         
+    printf("w = %d, h = %d, biSI = %d\n", ih.width, ih.height, ih.biSizeImage);
+    int i;
+    
+    IMAGE im;
+    for(i=0;i<9;i++)
+    {
+    	fread(&im, sizeof(unsigned char), sizeof(IMAGE), img);
+    	printf("r = %d, g = %d, b = %d\n", im.r, im.g, im.b);
+    }
+   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
